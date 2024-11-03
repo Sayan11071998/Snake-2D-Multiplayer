0aea902e0cdab5ef4c194dbcade1b682a0205ad3
Sayan Nandi <sayannandi11071998@gmail.com>
2024-11-04 04:32:31 +0530

    AudioController

--- Assets/Scripts/Audio/AudioController.cs
+++ Assets/Scripts/Audio/AudioController.cs
@@ -1,27 +1,46 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
 
-// [RequireComponent(typeof(AudioSource))]
-// public class AudioController : MonoBehaviour
-// {
-//     private static AudioController instance;
-//     public static AudioController Instance;
+public enum Sounds
+{
+    Eat = 0,
+    Poison = 1,
+    Death = 3
+}
 
-//     public AudioSource audioSourceBGM;
-//     public AudioSource audioSourceSFX;
+[RequireComponent(typeof(AudioSource))]
+public class AudioContoller : MonoBehaviour
+{
+    public AudioClip eat;
+    public AudioClip poison;
+    public AudioClip death;
 
+    private AudioSource _audioSource;
 
+    void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.playOnAwake = false;
+    }
 
+    public void Play(Sounds sound)
+    {
+        switch (sound)
+        {
+            case Sounds.Eat:
+                _audioSource.clip = eat;
+                break;
+            case Sounds.Poison:
+                _audioSource.clip = poison;
+                break;
+            case Sounds.Death:
+                _audioSource.clip = death;
+                break;
+        }
 
-
-
-
-// }
-
-// public enum AudioTypeList
-// {
-//     Eat,
-//     Poison,
-//     Death
-// }
+        _audioSource.pitch = Random.Range(0.95f, 1.1f);
+        _audioSource.Play();
+    }
+}
